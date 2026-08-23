8b09545197105e91498c57bff7e59d26cc06e995
Utkarsh Sharma <utkarsharma.189@gmail.com>
2026-08-23 17:25:00 +0530

    Time: 3 ms (37.18%) | Memory: 42.1 MB (33.81%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -12,6 +12,9 @@ public:
             pre[i]=pre[i-1]*nums[i-1];
             suf[n-i-1]=suf[n-i]*nums[n-i];
         }
+        for(int i = n-2;i<0;i--){
+            suf[i]=suf[i+1]*nums[i+1];
+        }
         for(int i = 0; i<n; i++){
             ans[i]=pre[i]*suf[i];
         }
